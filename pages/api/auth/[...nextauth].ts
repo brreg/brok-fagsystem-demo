7973edc5778d1c81677bf4da4271b1444d396e3e
Øyvind Hatland <oha@brreg.no>
2023-03-13 14:32:23 +0100

    login with email and magic link

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,15 @@
-import NextAuth from "next-auth"
-import EmailProvider from "next-auth/providers/email"
+import NextAuth from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../lib/mongodb/client";
 
 export default NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     EmailProvider({
       server: JSON.parse(process.env.SMTP_SERVER_CONFIG!),
       from: process.env.EMAIL_FROM,
     }),
   ],
-})
+});
